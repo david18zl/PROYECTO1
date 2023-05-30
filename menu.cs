f2843b93e5a900438991a7da799f511b652bb15c
david18zl <124113346+david18zl@users.noreply.github.com>
2023-05-29 23:40:02 -0600

    Update menu.cs

--- menu.cs
+++ menu.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Menusito
+namespace Menu
 {
     class program
     {
@@ -15,27 +15,27 @@ namespace Menusito
             {
                 //Menu de opciones 
                 Console.Clear();
-                Console.WriteLine("----MENU de tienda de Anime----");
-                Console.WriteLine("1-) OPCION de playera 1");
-                Console.WriteLine("2-) OPCION de juguetes 2");
-                Console.WriteLine("3-) OPCION llaveos 3");
+                Console.WriteLine("----MENU de tienda de playeras ----");
+                Console.WriteLine("1-) OPCION de playeras de compresion 1");
+                Console.WriteLine("2-) OPCION de playeras tipo overside 2");
+                Console.WriteLine("3-) OPCION de playeras a la medida 3");
                 Console.WriteLine("4-) OPCION de salir 4");
                 Console.WriteLine("INGRESA UNA OPCION PARA CONTINUAR");
                 int opcion = Convert.ToInt32(Console.ReadLine());
                 switch (opcion)
                 {
                     case 1:
-                        Console.WriteLine("Has entrado a la opcion playeras 1");
+                        Console.WriteLine("Has entrado a la opcion playeras de compresion 1");
                         Console.WriteLine("presiona cualquier tecla para continuar");
                         Console.ReadKey();
                         break;
                     case 2:
-                        Console.WriteLine("Has entrado a la opcion juguetes 2");
+                        Console.WriteLine("Has entrado a la opcion playeras tipo overside 2");
                         Console.WriteLine("presiona cualquier tecla para continuar");
                         Console.ReadKey();
                         break;
                     case 3:
-                        Console.WriteLine("Has entrado a la opcion llaveros 3");
+                        Console.WriteLine("Has entrado a la opcion playeras a la medida 3");
                         Console.WriteLine("presiona cualquier tecla para continuar");
                         Console.ReadKey();
                         break;
@@ -49,7 +49,7 @@ namespace Menusito
                         }
                         break;
                     default:
-                        Console.WriteLine("Usted ha ingresado una opcion no valida initente de nuevo . :");
+                        Console.WriteLine(" initente de nuevo . :");
                         break;
 
                 }
